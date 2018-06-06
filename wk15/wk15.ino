7b83f9f8335dda50cd03c88d07b7f74aba6a757c
alicevvv <39110488+alicevvv@users.noreply.github.com>
2018-06-06 11:10:04 +0800

    Add files via upload

---
+++ wk15/wk15.ino
@@ -0,0 +1,21 @@
+
+
+void setup() {
+
+// initialize serial communications at 9600 bps:
+
+Serial.begin(9600);}
+
+void loop() {
+
+int sensorRead = analogRead(A0);
+
+  if(sensorRead< 135){ sensorRead=135;}
+  else if(sensorRead > 186){sensorRead=186;}
+  int ledDimming = map(sensorRead, 135, 186, 1, 255);
+  analogWrite(3, ledDimming);
+  Serial.println(ledDimming);
+  delay(3000);
+
+
+}
